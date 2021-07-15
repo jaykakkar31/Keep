80ad2eafa36fa9903ab090fb721f97732f14323a
jaykakkar31 <jaykakkar31@gmail.com>
2021-07-16 01:41:52 +0530

    changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-// let cors = require("cors");
+let cors = require("cors");
 const bcrypt = require("bcrypt"); //3
 const saltRounds = 14;
 // const passport =require("passport-local")
