6eb04cc9bb3646a64e4970403f5cbb461bec4bca
jaykakkar31 <jaykakkar31@gmail.com>
2021-07-15 23:56:03 +0530

    changes

--- server.js
+++ server.js
@@ -314,6 +314,7 @@ app.post("/register", (req, res) => {
 
 
 if(process.env.NOD_ENV==='production'){
+    
     app.use(express.static('./client/build'))
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'./client/build','index.html'))
