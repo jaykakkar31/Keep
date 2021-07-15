e6873f4534651289bd94be2288966f3c6e471af4
jaykakkar31 <jaykakkar31@gmail.com>
2021-07-16 01:11:33 +0530

    commit

--- server.js
+++ server.js
@@ -313,12 +313,13 @@ app.post("/register", (req, res) => {
 });
 
 
-if(process.env.NOD_ENV==='production'){
-    
-    app.use(express.static('./client/build'))
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'./client/build','index.html'))
-    })
+if (process.env.NODE_ENV === "production") {
+	// Serve any static files
+	app.use(express.static(path.join(__dirname, "client/build")));
+	// Handle React routing, return all requests to React app
+	app.get("*", function (req, res) {
+		res.sendFile(path.join(__dirname, "client/build", "index.html"));
+	});
 }
 
 app.listen(port, () => {
